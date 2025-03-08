32ac0003f081c8996e11c4ec6d23e877e4f8749f
kunjalbhudke <kunjalbhudke04@gmail.com>
2025-03-08 17:50:13 +0530

    Updated HTML, CSS, and JavaScript files

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 
-
-
 document.getElementById("signIn").addEventListener("click", function () {
     window.location.href = "signin.html";
   });
+
+  function openNav(){
+    document.getElementById("mysidebar").style.width="365px";
+    document.getElementById("#main").style.marginLeft="365px";
+  }
+
+  function closeNav(){
+    document.getElementById("mysidebar").style.width="0";
+    document.getElementById("#main").style.marginLeft="0";
+  }
+
+  
+
+function show_location() {
+  const loc = document.querySelector(".location");
+  const overlay = document.getElementById("overlay");
+
+  loc.classList.add("Location");  
+  overlay.style.display = "block"; 
+}
+
+
+function CLose_Location() {
+  const loc = document.querySelector(".location");
+  const overlay = document.getElementById("overlay");
+
+  loc.classList.remove("Location");  
+  overlay.style.display = "none"; 
+}
+
+let signin = document.getElementById("sign-in");
+signin.addEventListener("click", function () {
+    window.location.href = "signin.html";
+  });
+
